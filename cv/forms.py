726fa792a49379e51329d8258504fab9e5a621d7
Jonas Schäfer <jonas.schaefer00@gmail.com>
2020-09-01 00:47:06 +0200

    Undo a lot of work, CV collection appears to not work, object references show None when they should now

--- cv/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import *
-
-class CvForm(forms.ModelForm):
-
-   class Meta:
-      model = Cv
-      fields = '__all__'
--- cv/views.py
+++ cv/views.py
@@ -3,25 +3,28 @@ from django.http import Http404
 from django.utils import timezone
 
 from .models import *
-from .forms import CvForm
+#from .forms import CvForm
 
 # Create your views here.
 def cv_view(request):
-   obj = Cv.objects.first()
-   if not obj:
+   work_experiences = WorkExperience.objects.all()
+   projects = Project.objects.all()
+   skills = Skill.objects.all()
+   education = Education.objects.all()
+   if not (work_experiences or projects or skills or education):
       raise Http404
-   cv = get_object_or_404(Cv, pk=obj.pk)
-   return render(request, 'cv/cv.html', {'cv': cv})
+   #cv = get_object_or_404(Cv, pk=obj.pk)
+   return render(request, 'cv/cv.html', {'works': work_experiences, 'projects': projects, 'skills': skills, 'education': education})
    
-def cv_edit(request):
-   cv = get_object_or_404(Cv)
-   if request.method == "POST":
-      form = CvForm(request.POST, instance=cv)
-      if form.is_valid():
-         cv = form.save(commit=False)
-         cv.last_updated = timezone.now()
-         cv.save()
-         return redirect('/cv', pk=cv.id)
-   else:
-      form = CvForm()
-   return render(request, 'blog/post_edit.html', {'form': form})
+#def cv_edit(request):
+#   cv = get_object_or_404(Cv)
+#   if request.method == "POST":
+#      form = CvForm(request.POST, instance=cv)
+#      if form.is_valid():
+#         cv = form.save(commit=False)
+#         cv.last_updated = timezone.now()
+#         cv.save()
+#         return redirect('/cv', pk=cv.id)
+#   else:
+#      form = CvForm()
+#   return render(request, 'blog/post_edit.html', {'form': form})
